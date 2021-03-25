7cc06c6793617cffd4e125f23f300fbf456004a7
Clare Henderson <henderson2350@gmail.com>
2021-03-25 12:32:19 -0400

    added variable for current hour and basic formatting for the table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
+// DOM VARIABLES
+// var eventsColumn = $("eventsColumn")
+// GLOBAL VARIABLES
 // Here we set a variable for today's date, then gather that information
 // formatted how we want it usuing the moment.js function.
 // We then use jQuery to assign the variable todaysDate to the id "date"
-var todaysDate = moment().format("MMM Do, YYYY");
-$("#date").text(todaysDate)
+var currentDay = moment().format("MMM Do, YYYY");
+$("#currentDay").text(currentDay)
+
+// here we gather the current hour
+var currentHour = moment().format("h")
+console.log(currentHour)
+
+// we will need to pass in 
+var currentColumn = $("#eventsColumn")
+eventsColumn.attr("class", "present")
+
+// FUNCTION DEFINITIONS
+// FUNCTION CALLS
+
