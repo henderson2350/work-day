f585cc30d5dec3e8a37d938aa6eb5dd8500f883a
Clare Henderson <henderson2350@gmail.com>
2021-03-25 15:22:20 -0400

    finished function for time blocks to change colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,26 @@
 // DOM VARIABLES
-// var eventsColumn = $("eventsColumn")
+var timeBlocks = $(".time-block")
+
 // GLOBAL VARIABLES
 // Here we set a variable for today's date, then gather that information
 // formatted how we want it usuing the moment.js function.
 // We then use jQuery to assign the variable todaysDate to the id "date"
 var currentDay = moment().format("MMM Do, YYYY");
+var currentHour = moment().format("h")
+
+// DOM MANIPULATION
 $("#currentDay").text(currentDay)
 
-// here we gather the current hour
-var currentHour = moment().format("h")
-console.log(currentHour)
+timeBlocks.each(function(i, element) {
+    var hour = ($(element).attr("id").replace("hour-",""));
+    if (hour===currentHour) {
+        $(element).addClass("present")
+    } else if (hour > currentHour && hour >= 9) {
+        $(element).addClass("past")
+    } else if (hour < currentHour) {
+        $(element).addClass("past")
+    } else if (hour > currentHour) {
+        $(element).addClass("future")
+    }
 
-// we will need to pass in 
-var currentColumn = $("#eventsColumn")
-eventsColumn.attr("class", "present")
-
-// FUNCTION DEFINITIONS
-// FUNCTION CALLS
-
+})
